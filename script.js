d1f299c117cb598a2ae5f8d427866c9340777eb2
angelob18 <155848677+angelob18@users.noreply.github.com>
2024-01-15 20:00:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'Ayy que riko 😮‍💨';
+    question.innerHTML = 'Diablos 😳';
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
